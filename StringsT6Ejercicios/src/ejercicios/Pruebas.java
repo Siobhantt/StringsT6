ca64a9545e793313ffcea09168c138c6bd1075f4
Siobhantt <luisa.cardozo@iesnervion.es>
2023-02-19 22:48:25 +0100

    Prueba del ejercicio4

--- StringsT6Ejercicios/src/ejercicios/Pruebas.java
+++ StringsT6Ejercicios/src/ejercicios/Pruebas.java
@@ -1,9 +1,15 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Pruebas {
 
 	public static void main(String[] args) {
-	String palabra = "luisa";
+		String palabra;
+		Scanner lee = new Scanner(System.in);
+		System.out.println("Por favor introduzca la palbra que quiere poner al reves: ");
+	palabra = lee.nextLine();
+			
 	
 		System.out.println(FuncionEj4.alReves(palabra));
 
