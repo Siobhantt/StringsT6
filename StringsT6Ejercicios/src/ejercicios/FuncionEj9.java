f4847e9099f40903260042857a837e321d7761b2
Siobhantt <luisa.cardozo@iesnervion.es>
2023-02-20 22:31:04 +0100

    Tengo muchas preguntas

---
+++ StringsT6Ejercicios/src/ejercicios/FuncionEj9.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class FuncionEj9 {
+	
+	public static char conjunto1 [] = {'e','i','k','m','p','q','r','s','t','u','v'};
+	public static char conjunto2 [] = {'p','v','i','u','m','t','e','r','k','q','s'};
+	
+	public static char codifica(char conjunto1[],char conjunto2[], char c) {
+		
+		
+	}
+
+}
