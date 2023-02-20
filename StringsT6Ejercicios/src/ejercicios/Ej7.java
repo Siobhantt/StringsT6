aa0aebaa42582e2dc0e0a61dd2839d878ee789e5
Siobhantt <luisa.cardozo@iesnervion.es>
2023-02-20 21:30:56 +0100

    Me ha gustado este ejercicio

--- StringsT6Ejercicios/src/ejercicios/Ej7.java
+++ StringsT6Ejercicios/src/ejercicios/Ej7.java
@@ -1,36 +1,41 @@
 package ejercicios;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ej7 {
-/*Realizar un programa que lea una frase del teclado y nos indique si es palíndroma, 
- * es decir, que la frase sea igual leyendo de izquierda a derecha, que de derecha a izquierda, sin tener en cuenta los espacios. 
- * Un ejemplo de frase palíndroma es: “Dábale arroz a la zorra el abad”. 
- * Las vocales con tilde hacen que un algoritmo tome una frase palíndroma como si no lo fuese. 
- * Por esto, supondremos que el usuario introduce la frase sin tildes.*/
-	
+	/*
+	 * Realizar un programa que lea una frase del teclado y nos indique si es
+	 * palíndroma, es decir, que la frase sea igual leyendo de izquierda a derecha,
+	 * que de derecha a izquierda, sin tener en cuenta los espacios. Un ejemplo de
+	 * frase palíndroma es: “Dábale arroz a la zorra el abad”. Las vocales con tilde
+	 * hacen que un algoritmo tome una frase palíndroma como si no lo fuese. Por
+	 * esto, supondremos que el usuario introduce la frase sin tildes.
+	 */
+
 	public static void main(String[] args) {
-	
-		String frase[] = new String[0];
-		String alReves[]=new String[0];
+
+		String frase;
+		int longitud;
+		char tablaFrase[];
+
 		Scanner lee = new Scanner(System.in);
-		String cad1;
-		String cad2;
 		
 		System.out.println("Por favor introduzca la frase a verificar: ");
-		
-		for(int i=0;i<frase.length-1;i++) {
-			frase[i] = lee.next();
+		frase = lee.nextLine(); //leemos la frase
+		frase = frase.replace(" ", ""); //reemplazamos los espacios en blanco por espacios vacios
+		longitud = frase.length(); //tomamos la longitud de la frase
+
+		char[] alReves = new char[longitud]; //a la tabla que pondremos al reves le damos la longitud de la frase
+		tablaFrase = frase.toCharArray(); //y en tabla frase guardaremos la frase pero dividida en caracteres individuales
+		for (int i = 0; i < longitud; i++) { //hacemos un for donde va a iterar la cantidad que se la longitud de la frase
+			alReves[i] = tablaFrase[longitud - 1 - i]; //y en la tabla al reves iremos guardando el ultimo elemento de la tabla principal
 		}
-		
-		for(int i=0;i<alReves.length;i++) {
-			alReves[i] = frase[frase.length-1];
-		}
-		
-		cad1 = String.valueOf(frase);
-		cad2 = String.valueOf(alReves);
-		
-		//System.out.println(cad1.equalsIgnoreCase(cad2) ? "La frase es palindromo" : "La frase no es palindromo");
+		System.out.println(Arrays.toString(tablaFrase));
+		System.out.println(Arrays.toString(alReves));
+		System.out.println(Arrays.equals(tablaFrase, alReves) ? "La frase es palindromo" : "La frase NO es palindromo");
+
+		lee.close();
 	}
 
 }
