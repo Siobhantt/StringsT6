1f633a0bb8fc93f046a4c6b9c9e5f764eaa9368c
lcardozo <lcardozo@106-25.info2>
2023-02-20 13:36:45 +0100

    Que rabia me da esta vaina

--- StringsT6Ejercicios/src/ejercicios/Ej7.java
+++ StringsT6Ejercicios/src/ejercicios/Ej7.java
@@ -1,5 +1,7 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ej7 {
 /*Realizar un programa que lea una frase del teclado y nos indique si es palíndroma, 
  * es decir, que la frase sea igual leyendo de izquierda a derecha, que de derecha a izquierda, sin tener en cuenta los espacios. 
@@ -8,8 +10,27 @@ public class Ej7 {
  * Por esto, supondremos que el usuario introduce la frase sin tildes.*/
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		String frase[] = new String[0];
+		String alReves[]=new String[0];
+		Scanner lee = new Scanner(System.in);
+		String cad1;
+		String cad2;
+		
+		System.out.println("Por favor introduzca la frase a verificar: ");
+		
+		for(int i=0;i<frase.length-1;i++) {
+			frase[i] = lee.next();
+		}
+		
+		for(int i=0;i<alReves.length;i++) {
+			alReves[i] = frase[frase.length-1];
+		}
+		
+		cad1 = String.valueOf(frase);
+		cad2 = String.valueOf(alReves);
+		
+		//System.out.println(cad1.equalsIgnoreCase(cad2) ? "La frase es palindromo" : "La frase no es palindromo");
 	}
 
 }
