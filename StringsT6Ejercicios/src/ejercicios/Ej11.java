46dfbf2b1c29d00ffda6973f5392cf0b64a3b1f4
Siobhantt <luisa.cardozo@iesnervion.es>
2023-02-23 19:43:07 +0100

    Listo el ejercicio11, comentado.

--- StringsT6Ejercicios/src/ejercicios/Ej11.java
+++ StringsT6Ejercicios/src/ejercicios/Ej11.java
@@ -9,31 +9,28 @@ public class Ej11 {
 	 * una vez hecho eso analizar la longitud de lo que esta guardado en lai y lo que sea myor pues es la palabra mas grande*/
 	public static void main(String[] args) {
 		
-		String frase;
+		String frase;//la frase que va a introducir el usuario
 		Scanner lee = new Scanner(System.in);
-		String palabras[];
-		int longitud;
-		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
-		String mayor="";
+		String palabras[];//el array donde voy guardar la frase para asi dividirlo
+		int longitud=0;//la longitud de las palabras para asi comparar cual tiene mayor cantidad de caracteres
+		int maxCantLetras = Integer.MIN_VALUE; //la cantidad maxima de letras que tenga una palabra 
+		String palabraMayor=""; //aqui vy a guardar la palabra con mas letras
 		
 		System.out.println("Por favor introduzca una frase: ");
 		frase = lee.nextLine();
-		
+		//uso el split para separar la frase, el punto de separacion serán los espacios
 		palabras = frase.split(" ");
-		
+		//recorro la tabla que contiene las palabras de la frase
 		for(int i =0;i<palabras.length;i++) {
-			longitud = palabras[i].length();
-				if (longitud > max) {
-					mayor = palabras[i];
-					
-				}
+			longitud = palabras[i].length();//en esta variable guardo la longitud de la palabra que este guardada en i en esa iteracion
+			//System.out.println(palabras[i] + ": "+ longitud);
+			if(longitud>maxCantLetras) { //si la longitud de la palabra es mayor que la cantidad mayor de leras guardada
+				palabraMayor = palabras[i]; //se establece como palabra mayor 
+				maxCantLetras=longitud;//y la longitud de esa palabra se establece como el nuevo maximo
 			}
-		System.out.println(mayor);
-	
 		}
-		
-		
+		System.out.println("La palabra más larga de la frase es: " + palabraMayor + " \ny la longitud de esa palabra es: "+ maxCantLetras);
+		}
 	}
 
 
