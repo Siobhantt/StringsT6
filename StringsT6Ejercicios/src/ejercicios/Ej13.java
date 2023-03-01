08b8c7f3223a441606493408b074713604376ce6
Siobhantt <luisa.cardozo@iesnervion.es>
2023-03-01 16:00:21 +0100

    Ej13 faltan comentarios

--- StringsT6Ejercicios/src/ejercicios/Ej13.java
+++ StringsT6Ejercicios/src/ejercicios/Ej13.java
@@ -1,7 +1,11 @@
 package ejercicios;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Ej13 {
-/*Diseñar un algoritmo que lea del teclado una frase e indique, para la letras que aparecen en la frase, cuántas veces se repite cada una. Se consideran iguales las letras mayúsculas y las minúsculas para realizar la cuenta. Un ejemplo sería:
+/*Diseñar un algoritmo que lea del teclado una frase e indique, para la letras que aparecen en la frase, cuántas veces se repite cada una. 
+ * Se consideran iguales las letras mayúsculas y las minúsculas para realizar la cuenta. Un ejemplo sería:
 Frase : En un lugar de la Mancha .
 Resultado :
 a: 4 veces
@@ -9,12 +13,31 @@ d: 1 vez
 e: 2 veces
 . . .
 */
+
 	public static void main(String[] args) {
 		String frase;
+		char []tablaFrase;
+		char [] abecedario = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'};
 		Scanner lee = new Scanner(System.in);
+		int contadorLetra =0;
 		
-		
-
-	}
-
+		System.out.println("Por favor introduzca una frase: ");
+		frase = lee.nextLine().toLowerCase();
+		frase = frase.replace(" ", "");
+		tablaFrase =frase.toCharArray();
+	
+		for(int i=0;i<abecedario.length;i++) {
+			for(int j=0;j<tablaFrase.length;j++) {
+				if(abecedario[i] == tablaFrase[j]) {
+					contadorLetra++;
+				}//fin del if	
+			}
+			if(contadorLetra!=0) {
+				System.out.println("La letra " +	abecedario[i] + " se repite " + contadorLetra);
+			}
+			contadorLetra =0;
+		}
+	
+		lee.close();
+	}//finn del main
 }
---
+++ StringsT6Ejercicios/src/ejercicios/Ej14.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+public class Ej14 {
+/*Implementar el juego del anagrama, que consiste en que un jugador escribe una palabra o frase, 
+ * y la aplicación muestra un anagrama (transposición de los caracteres) del texto introducido generado al azar. 
+ * A continuación otro jugador tiene que acertar cuál es el texto original. 
+ * La aplicación no debe permitir que el texto introducido por el jugador 1 sea la cadena vacía.
+ *  Por ejemplo, si el jugador 1 escribe “teclado”, la aplicación muestra como pista un anagrama al azar: “etcloda”. 
+ *  Puedes usar una función que realice la desordenación del texto.*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
