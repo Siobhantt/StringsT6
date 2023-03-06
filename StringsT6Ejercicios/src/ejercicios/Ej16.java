ccd80ae6cc1a901c647d7261fef583f9bcdc1cb7
Siobhantt <luisa.cardozo@iesnervion.es>
2023-03-06 21:14:13 +0100

    Ejercicio 1 boletin 2 strings

--- StringsT6Ejercicios/src/ejercicios/Ej16.java
+++ StringsT6Ejercicios/src/ejercicios/Ej16.java
@@ -1,5 +1,7 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ej16 {
 /*El preprocesador del lenguaje C elimina los comentarios (/* … */// del código fuente antes de compilar. 
 	//Diseña un programa que lea por teclado una sentencia en C, y elimine los comentarios:
@@ -7,6 +9,13 @@ public class Ej16 {
 //Salida: if (a==3) a++;
 
 	public static void main(String[] args) {
+		Scanner lee = new Scanner (System.in);
+		String frase;
+		
+		System.out.println("Por favor introduzca una sentencia en C: ");
+		frase = lee.nextLine();
+		frase= frase.replaceAll("\\/\\*.*?\\*\\/", "");
+        System.out.println("Sentencia sin comentarios: " + frase);
 		
 
 	}
---
+++ StringsT6Ejercicios/src/ejercicios/Ej19.java
@@ -0,0 +1,11 @@
+package ejercicios;
+
+public class Ej19 {
+/*Implementa un programa que lea una frase y muestre todas sus palabras ordenadas de forma alfabética. 
+ * Suponemos que cada palabra de la frase se separa de otra por un único espacio.*/
+	public static void main(String[] args) {
+		
+
+	}
+
+}
