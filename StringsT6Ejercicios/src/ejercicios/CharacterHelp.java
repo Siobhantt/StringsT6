480d52ffeef049cf87550b941abe9a48b604060d
Siobhantt <luisa.cardozo@iesnervion.es>
2023-03-06 20:55:35 +0100

    Ayuda clase Character

--- StringsT6Ejercicios/src/ejercicios/CharacterHelp.java
+++ StringsT6Ejercicios/src/ejercicios/CharacterHelp.java
@@ -9,23 +9,23 @@ Aquí os dejo una lista de los principales métodos de la clase Character. La ma
 Método.																																			 	Descripción.
 compare(char x, char y) 																							Compara dos caracteres numéricamente.
 compareTo(Character anotherCharacter) 																Compara dos objetos de la clase Character – (retorna int)
-digit(char ch, int radix) 	Devuelve el valor numérico del carácter con int especificado.
-getNumericValue(char ch) 	Devuelve el valor entero que representa el carácter especificado (punto de código Unicode).
-getNumericValue(int codePoint) 	Devuelve el valor absoluto de un valor long introducido como parámetro.
-getType(char ch) 	Devuelve un valor que indica el tipo de carácter.
-getType(int codePoint) 	Devuelve un valor que indica el tipo de carácter.
-hashCode(char value) 	Devuelve un código hash para un carácter.
-isAlphabetic(int codePoint) 	Determina si el carácter especificado (punto de código Unicode) es un alfabeto.
-isDigit(char ch) 	Determina si el carácter especificado es un dígito.
-isLetter(char ch) 	Determina si el carácter especificado es una letra.
-isLetterOrDigit(char ch) 	Determina si el carácter especificado es una letra o un dígito.
-isLowerCase(char ch) 	Determina si el carácter especificado es un carácter escrito en minúscula.
-isUpperCase(char ch) 	Determina si el carácter especificado es un carácter escrito en mayúscula.
-isWhitespace(char ch) 	Determina si el carácter especificado es un espacio en blanco.
-toLowerCase(char ch) 	Convierte el caracter a minúsculas.
-toString(char c) 	Devuelve un objeto o cadena String del carácter pasado como argumento.
-toUpperCase(char ch) 	Convierte el carácter a mayúsculas.
-valueOf(char c) 	Devuelve un objeto del carácter pasado como argumento.*/
+digit(char ch, int radix) 																								Devuelve el valor numérico del carácter con int especificado.
+getNumericValue(char ch) 																						Devuelve el valor entero que representa el carácter especificado (punto de código Unicode).
+getNumericValue(int codePoint) 																			Devuelve el valor absoluto de un valor long introducido como parámetro.
+getType(char ch) 																										Devuelve un valor que indica el tipo de carácter.
+getType(int codePoint) 																							Devuelve un valor que indica el tipo de carácter.
+hashCode(char value) 																								Devuelve un código hash para un carácter.
+isAlphabetic(int codePoint) 																						Determina si el carácter especificado (punto de código Unicode) es un alfabeto.
+isDigit(char ch) 																											Determina si el carácter especificado es un dígito.
+isLetter(char ch) 																										Determina si el carácter especificado es una letra.
+isLetterOrDigit(char ch) 																							Determina si el carácter especificado es una letra o un dígito.
+isLowerCase(char ch) 																								Determina si el carácter especificado es un carácter escrito en minúscula.
+isUpperCase(char ch) 																								Determina si el carácter especificado es un carácter escrito en mayúscula.
+isWhitespace(char ch) 																								Determina si el carácter especificado es un espacio en blanco.
+toLowerCase(char ch) 																								Convierte el caracter a minúsculas.
+toString(char c) 																											Devuelve un objeto o cadena String del carácter pasado como argumento.
+toUpperCase(char ch) 																								Convierte el carácter a mayúsculas.
+valueOf(char c) 																											Devuelve un objeto del carácter pasado como argumento.*/
 
 	}
 
